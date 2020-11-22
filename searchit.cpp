ea2fef38c1bae127033d3198480552974d3d9e53
imad <58376083+imad-1999@users.noreply.github.com>
2020-11-22 23:38:31 +0100

    Add files via upload

---
+++ searchit.cpp
@@ -0,0 +1,15 @@
+
+
+
+      int SearchKey(int k) {
+         int h = HashFunc(k);
+         while (t[h] != NULL && t[h]->k != k) {
+            h = HashFunc(h + 1);
+         }
+         if (t[h] == NULL)
+            return -1;
+         else
+            return t[h]->v;
+      }
+      
+
